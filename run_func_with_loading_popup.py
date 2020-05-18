19e120d6cac889526e3f70fa9c0190e32dd2b45f
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-05-17 21:18:12 -0500

    added run_as_master to loading popup

--- run_func_with_loading_popup.py
+++ run_func_with_loading_popup.py
@@ -10,7 +10,7 @@ import GUI_tools_utils as gtu
 # the given message with a bouncing progress bar will appear for as long as func is running, returns same as if func was run normally
 # a pb_length of None will result in the progress bar filling the window whose width is set by the length of msg
 # Ex:  run_func_with_loading_popup(lambda: task('joe'), photo_img_path)  
-def run_func_with_loading_popup(func, msg, window_title = None, bounce_speed = 8, pb_length = None, photo_img_path = None, app_id = None):
+def run_func_with_loading_popup(func, msg, window_title = None, bounce_speed = 8, pb_length = None, photo_img_path = None, app_id = None, run_as_master = False):
     func_return_l = []
  
     class Main_Frame(object):
@@ -70,7 +70,10 @@ def run_func_with_loading_popup(func, msg, window_title = None, bounce_speed = 8
 
     # highest level GUI must always use TK(), not Toplevel(), PhotoImage can only work after TK(), but if this has already been called in a higher level GUI, use Toplevel()
     # running with Toplevel as your root GUI will also make a blank window appear
-    root = Toplevel() 
+    if run_as_master:
+        root = Tk()
+    else:
+        root = Toplevel() 
     
     gtu.set_child_tk_gui_iconphoto_and_app_id(root, photo_img_path, app_id)
     
@@ -85,6 +88,81 @@ def run_func_with_loading_popup(func, msg, window_title = None, bounce_speed = 8
     
     
 if __name__ == '__main__':
+#     import time
+#     def task(i):
+#         # The window will stay open until this function call ends.
+#         for x in range(10):
+#             print('hi: ' + i)
+#             time.sleep(.5) # Replace this with the code you want to run
+#         return "this is the func return"
+#          
+#     msg = 'running func...'        
+#          
+#     bounc_speed = 9
+#     pb_length = 200
+#     window_title = "Wait"
+# #     photo_img_path = "C:\\Users\\mt204e\\Documents\\projects\\Bitbucket_repo_setup\\version_control_scripts\\CE\\imgs\\git.png"
+#          
+#     r = run_func_with_loading_popup(lambda: task('joe'), msg, window_title, bounc_speed, pb_length)
+#  
+#     print('return of test: ', r)    
+
+    
+     
+    import keyboard as k
+    from pynput.keyboard import Key
+    from pynput.keyboard import Controller
+    from pynput.keyboard import Listener
+    import time
+    import _tkinter
+    from pynput import keyboard
+    from func_timeout import func_timeout, FunctionTimedOut
+     
+    def any_key_pressed(timeout = 0.1):
+        def wait_for_key_pressed():
+            def on_press(key):
+                return False
+              
+            def on_release(key):
+                return False
+          
+              
+            listener = keyboard.Listener(
+                on_press=on_press,
+                on_release=on_release)
+            listener.start()
+            listener.join()
+     
+        try:
+            doitReturnValue = func_timeout(timeout, wait_for_key_pressed)
+            return True
+          
+        except FunctionTimedOut:
+            return False
+          
+        except Exception as e:
+            raise
+         
+         
+    def wait_until_no_keys_pressed(to_return = "TEST RETURN !!!!!!!!!!!"):
+        while(any_key_pressed() == True):
+#             print('waiting for no')
+            pass
+        return to_return
+          
+          
+          
+    run_func_with_loading_popup(wait_until_no_keys_pressed, msg = 'hi', window_title = 'title', bounce_speed = 8, pb_length = None, photo_img_path = None, app_id = None, run_as_master = True)
+          
+#     wait_until_no_keys_pressed()
+          
+          
+          
+          
+          
+
+
+
     import time
     def task(i):
         # The window will stay open until this function call ends.
@@ -92,27 +170,24 @@ if __name__ == '__main__':
             print('hi: ' + i)
             time.sleep(.5) # Replace this with the code you want to run
         return "this is the func return"
-        
+         
     msg = 'running func...'        
-        
+         
     bounc_speed = 9
     pb_length = 200
     window_title = "Wait"
 #     photo_img_path = "C:\\Users\\mt204e\\Documents\\projects\\Bitbucket_repo_setup\\version_control_scripts\\CE\\imgs\\git.png"
-        
-    r = run_func_with_loading_popup(lambda: task('joe'), msg, window_title, bounc_speed, pb_length)
+         
+#     r = run_func_with_loading_popup(lambda: task('joe'), msg, window_title, bounc_speed, pb_length)
 
+    time.sleep(3) # remove !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#     r = run_func_with_loading_popup(wait_until_no_keys_pressed, msg, window_title, bounc_speed, pb_length)
+    r = run_func_with_loading_popup(wait_until_no_keys_pressed, msg, window_title, bounc_speed, pb_length)
+ 
     print('return of test: ', r)    
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-    
+         
+         
+         
+         
+         
+        
